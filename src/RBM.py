f019b1aa116903078febac73496ac61257aa869d
Adbias <fortunehouessou@gmail.com>
2025-03-27 18:53:48 -0400

    Rapport 27 mars

--- src/RBM.py
+++ src/RBM.py
@@ -17,7 +17,7 @@ class GaussianBinaryRBM(nn.Module):
     def sample_h(self, v):
         """Échantillonne la couche cachée à partir des visibles"""
         p_h_given_v = torch.sigmoid(torch.matmul(v, self.W.T) + self.h_bias)
-        return torch.bernoulli(p_h_given_v)
+        return torch.bernoulli(p_h_given_v), p_h_given_v
 
     def sample_v(self, h):
         """Échantillonne la couche visible à partir des cachées (distribution gaussienne)"""
@@ -32,30 +32,36 @@ class GaussianBinaryRBM(nn.Module):
     def contrastive_divergence(self, v0, k=1, lr=0.01):
         """Applique le Contraste de Divergence CD-k pour l'apprentissage"""
         v = v0.detach()
-        
+    
         # Propagation avant
-        h0 = self.sample_h(v) # phase positive
-        
+        h0, P_h_given_v0 = self.sample_h(v)  # phase positive
+    
         # Gibbs Sampling k étapes (phases négatives)
+        hk = h0
         for _ in range(k):
-            v = self.sample_v(h0)
-            h0 = self.sample_h(v)
+            vk = self.sample_v(hk)
+            hk, P_h_given_vk = self.sample_h(vk)
+    
+        # Mise à jour des poids de manière vectorisée
+        delta_h_bias = P_h_given_v0 - P_h_given_vk
+        self.h_bias.data += lr * delta_h_bias
 
-        # Mise à jour des poids
-        h0_v0 = torch.outer(h0, v0)
+        delta_v_bias = v0 - vk
+        self.v_bias.data += lr * delta_v_bias
 
-        h_k_v_k = torch.outer(h0, v)
+        delta_W = torch.outer(delta_h_bias, v0) - torch.outer(delta_h_bias, vk)
+        self.W.data += lr * delta_W
 
-        self.W.data += lr * (h0_v0 - h_k_v_k)
-        self.v_bias.data += lr * torch.mean(v0 - v, dim=0)
-        self.h_bias.data += lr * torch.mean(h0 - h0, dim=0)
+                
 
+        
+        
     def forward(self, v, iter=1):
         """Passe avant récursif pour raffiner les embeddings"""
         if iter <= 0:
             return v  # Arrêt de la récursion
     
-        h = self.sample_h(v)
+        h,_ = self.sample_h(v)
         v = self.sample_v(h)
     
         return self.forward(v, iter - 1)  # Appel récursif avec iter - 1
